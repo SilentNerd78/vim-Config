e37fa7d33d1f1f94c6835882ceb715af47737c7c
SilentNerd78 <94817882+SilentNerd78@users.noreply.github.com>
2022-07-15 20:28:27 +0600

    Delete lua directory

--- lua/colourschemes-config/gruvbox.lua
+++
@@ -1,5 +0,0 @@
-vim.o.background = "dark"
-vim.cmd([[
-    let g:gruvbox_contrast_dark="soft"
-    colorscheme gruvbox
-]])
--- lua/nvim-dap-config/virtual-text.lua
+++
@@ -1,6 +0,0 @@
-require("nvim-dap-virtual-text").setup {
-  commented = true,
-  virt_text_win_col = 80,
-  highlight_changed_variables = true,
-  show_stop_reason = true,
-}
